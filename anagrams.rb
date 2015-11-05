3aa5346a0561cc857998de4a8b7538a862cefcf4
App Academy Student <student@appacademy.io>
2015-11-05 14:28:31 -0800

    Done with all anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -10,19 +10,36 @@ def first_anagram?(first_string, second_string)
 end
 
 def second_anagram?(first_string, second_string)
+
+  duped_first_string = first_string.dup
+  duped_second_string = second_string.dup
   idx_1 = 0
   while idx_1 < first_string.length
-    idx_2 = 0
-    while idx_2 < second_string.length
-      if first_string[idx_1] == second_string[idx_2]
-        first_string.sub!(first_string[idx_1], "")
-        second_string.sub!(second_string[idx_2], "")
-        idx_1 -= 1
-        break
-      end
-      idx_2 += 1
-    end
+    duped_first_string.sub!(first_string[idx_1], "")
+    duped_second_string.sub!(first_string[idx_1], "")
     idx_1 += 1
   end
-  first_string.empty? && second_string.empty?
+  duped_first_string == duped_second_string
+end
+
+def third_anagram?(first_string, second_string)
+  sorted1 = first_string.split("").sort.join
+  sorted2  = second_string.split("").sort.join
+
+  sorted1 == sorted2
+end
+
+def fourth_anagram?(first_string, second_string)
+  h1_string = Hash.new(0)
+  h2_string = Hash.new(0)
+
+  idx = 0
+  while idx < first_string.length
+    h1_string[first_string[idx]] += 1
+    h2_string[second_string[idx]] += 1
+    idx += 1
+  end
+
+  h1_string == h2_string
+
 end
