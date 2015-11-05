312219d89deb2bd5dee89880355951ea2a21887f
App Academy Student <student@appacademy.io>
2015-11-05 13:50:07 -0800

    write second anagram method

--- anagrams.rb
+++ anagrams.rb
@@ -1,5 +1,4 @@
 def first_anagram?(first_string, second_string)
-
   anagrams = []
 
   letters = first_string.split("")
@@ -11,5 +10,19 @@ def first_anagram?(first_string, second_string)
 end
 
 def second_anagram?(first_string, second_string)
-
+  idx_1 = 0
+  while idx_1 < first_string.length
+    idx_2 = 0
+    while idx_2 < second_string.length
+      if first_string[idx_1] == second_string[idx_2]
+        first_string.sub!(first_string[idx_1], "")
+        second_string.sub!(second_string[idx_2], "")
+        idx_1 -= 1
+        break
+      end
+      idx_2 += 1
+    end
+    idx_1 += 1
+  end
+  first_string.empty? && second_string.empty?
 end
